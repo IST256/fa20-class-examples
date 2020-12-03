0d10274e435db7965435b0b5fb73607e0d24fcda
mafudge <mafudge@syr.edu>
2020-12-03 16:08:10 -0500

    update to nbenv

--- include/ist256/nbenvironment.py
+++ include/ist256/nbenvironment.py
@@ -1,15 +1,18 @@
 import os 
 import ipykernel
 import requests 
+import json
 
 class NbEnvironment(object):
     
     def __init__(self):
+        self.__settings = self.__load_settings()
         self.__netid = self.__find_netid()
         self.__notebook_path = self.__find_notebook_path()
         self.__service_prefix = self.__find_service_prefix()
         self.__course = self.__find_course()
         self.__git_folder = self.__find_git_folder()
+        self.__bucket = self.__find_bucket()
 
     @property
     def properties(self):
@@ -40,11 +43,26 @@ class NbEnvironment(object):
     @property 
     def git_folder(self):
         return self.__git_folder
+    
+    @property 
+    def bucket(self):
+        return self.__bucket
+    
+    def __load_settings(self):
+        if os.path.exists(".settings"):
+            with open(".settings","r") as f:
+                settings = json.load(f)
+                return settings
+        else:
+            return {}
 
+    def __find_bucket(self):
+        return f"{self.__course}-{self.__git_folder}"
+            
     def __find_git_folder(self):
         items = self.__notebook_path.split('/')
         if len(items) > 3 and items[0] == 'library':
-            return items[2]
+            return self.__settings.get('git-folder',items[2])
         else:
             raise Error('This notebook file is not in a git folder under the course folder.')
     
